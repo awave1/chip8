089a69bcb9b9b13ad02e0d35768d439d799015ed
awave1 <artgolovin96@gmail.com>
2018-09-04 23:38:40 -0600

    abstracts start functions and adds error handling + debug output

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,43 @@ use std::fs::File;
 mod cpu;
 use cpu::Cpu;
 
-fn main() {
-    println!("yo, chip-8");
-    let mut file = File::open("./data/space_invaders.ch8").unwrap();
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        if cfg!(debug_assertions) {
+            println!($($arg)*);
+        }
+    };
+}
+
+fn get_program_data(path: &str) -> Result<Vec<u8>, io::Error> {
+    let f = File::open(path);
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
     let mut program_data = Vec::<u8>::new();
-    file.read_to_end(&mut program_data).unwrap();
-    for mut b in program_data {
-        println!("{:x}", b);
+    match f.read_to_end(&mut program_data) {
+        Ok(_) => Ok(program_data),
+        Err(e) => Err(e),
+    }
+}
+
+fn start_with_data(data: Vec<u8>) {
+    println!("yo, chip-8");
+
+    for mut b in data {
+        debug!("{:x}", b);
     }
 
     let cpu = Cpu::new();
+}
+
+fn main() {
+    match get_program_data("./data/space_invaders.ch8") {
+        Ok(data) => start_with_data(data),
+        Err(e) => panic!("Oops, error opening the file {:?}", e),
+    }
+
+    println!("bye!");
 }
