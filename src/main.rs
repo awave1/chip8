c71df9bf14c4946457744d9a23086a472028f2be
awave1 <artgolovin96@gmail.com>
2018-09-04 22:31:17 -0600

    basic file read operation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-mod cpu;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
 
+mod cpu;
 use cpu::Cpu;
 
 fn main() {
     println!("yo, chip-8");
+    let mut file = File::open("./data/space_invaders.ch8").unwrap();
+    let mut program_data = Vec::<u8>::new();
+    file.read_to_end(&mut program_data).unwrap();
+    for mut b in program_data {
+        println!("{:x}", b);
+    }
+
     let cpu = Cpu::new();
-    cpu.start();
 }
